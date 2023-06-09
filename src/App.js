4c2560db360267b76d390e72a13da2b651018200
Jean Carlos Michetti <scan.michetti@voeazul.com.br>
2023-06-09 18:03:12 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Papa from 'papaparse';
 
 function App() {
+  const [csvData, setCsvData] = useState([]);
+  const [totalCol8, setTotalCol8] = useState(0);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    Papa.parse(file, {
+      complete: (results) => {
+        const data = results.data;
+        setCsvData(data);
+        calculateTotal(data);
+      },
+    });
+  };
+
+  const calculateTotal = (data) => {
+    let total = 0;
+    for (let i = 1; i < data.length; i++) {
+      const value = parseFloat(data[i][7]);
+      if (!isNaN(value)) {
+        total += value;
+      }
+    }
+    setTotalCol8(total);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Upload de Arquivo CSV</h1>
+      <input type="file" accept=".csv" onChange={handleFileUpload} />
+      <table>
+        <thead>
+          <tr>
+            <th>Coluna 1</th>
+            <th>Coluna 2</th>
+            <th>Coluna 3</th>
+            <th>Coluna 4</th>
+            <th>Coluna 5</th>
+            <th>Coluna 6</th>
+            <th>Coluna 7</th>
+            <th>Coluna 8</th>
+            <th>Coluna 9</th>
+          </tr>
+        </thead>
+        <tbody>
+          {csvData.map((row, index) => (
+            <tr key={index}>
+              {row.map((cell, cellIndex) => (
+                <td key={cellIndex}>{cell}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p>Total Coluna 8: {totalCol8}</p>
     </div>
   );
 }
